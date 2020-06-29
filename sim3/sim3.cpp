99c7ea0410f93003154cac2c2e03c81eb75c4efc
Edvin Boul <kittenparry@users.noreply.github.com>
2020-06-29 03:09:20 +0300

    Draw map with random food placement
    Create an int array for the whole map, 0 for empty spaces, -1 for food in random places
    Draw map with those values replaces with space and 'O'
    
    Commented out timer stuff for now

--- sim3/sim3.cpp
+++ sim3/sim3.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
 // interval of each refresh in time system below
 const int NUM_SECONDS = 1;
+void drawMap(int w, int h, int map[]);
 
 int main() {
+    int mapWidth = 10;
+    int mapHeight = 12;
+    int* mapContents = nullptr;
+    mapContents = new int[mapWidth * mapHeight];
+
+    for (int y = 0; y < mapHeight; y++) {
+        for (int x = 0; x < mapWidth; x++) {
+            mapContents[y * mapWidth + x] = 0;
+        }
+    }
+
+    srand(time(NULL));
+    int food = 15;
+    while (food) {
+        int rx = rand() % mapWidth;
+        int ry = rand() % mapHeight;
+
+        if (mapContents[ry * mapWidth + rx] == 0) {
+            mapContents[ry * mapWidth + rx] = -1;
+            food--;
+        }
+    }
+
+    drawMap(mapWidth, mapHeight, mapContents);
+
+    
+    // timer stuff below
+    /*
     int count = 1;
 
     double time_counter = 0;
@@ -31,5 +61,30 @@ int main() {
         }
 
         // printf("DebugTime = %f\n", time_counter);
+    }
+    */
+}
+
+void drawMap(int w, int h, int map[]) {
+    int tileVal;
+    char tileChar;
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            tileVal = map[y * w + x];
+
+            switch (tileVal) {
+            case 0:
+                tileChar = ' ';
+                break;
+            case -1: // food at the moment
+                tileChar = 'O';
+                break;
+            default:
+                break;
+            }
+
+            printf("(%c)", tileChar);
+        }
+        printf("\n");
     }
 }
